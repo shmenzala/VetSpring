ada38c4c42451ae20239b848ee737d4a6f4ed7f3
Samuel <shmenzala@gmail.com>
2023-02-21 12:10:14 -0500

    ResourceNotFoundException class para las excepciones

---
+++ Veterinaria/src/main/java/com/pe/sh/Veterinaria/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pe.sh.Veterinaria.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author shmen
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String nombreRecurso;
+    private String nombreCampo;
+    private String valorCampo;
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String nombreRecurso, String nombreCampo, String valorCampo) {
+        super(String.format("%s No encontrado con: %s : %s", nombreRecurso, nombreCampo, valorCampo));
+        this.nombreRecurso = nombreRecurso;
+        this.nombreCampo = nombreCampo;
+        this.valorCampo = valorCampo;
+    }
+
+    public String getNombreRecurso() {
+        return nombreRecurso;
+    }
+
+    public void setNombreRecurso(String nombreRecurso) {
+        this.nombreRecurso = nombreRecurso;
+    }
+
+    public String getNombreCampo() {
+        return nombreCampo;
+    }
+
+    public void setNombreCampo(String nombreCampo) {
+        this.nombreCampo = nombreCampo;
+    }
+
+    public String getValorCampo() {
+        return valorCampo;
+    }
+
+    public void setValorCampo(String valorCampo) {
+        this.valorCampo = valorCampo;
+    }
+    
+}
