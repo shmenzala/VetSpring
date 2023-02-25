afe1bcd440235a375cfbd4ac32c34636b32030e2
Samuel <shmenzala@gmail.com>
2023-02-25 12:52:19 -0500

    Añadir porperties de JWT (application.properties)

--- Veterinaria/src/main/java/com/pe/sh/Veterinaria/security/JwtAuthenticationEntryPoint.java
+++ Veterinaria/src/main/java/com/pe/sh/Veterinaria/security/JwtAuthenticationEntryPoint.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         //Al detectar un usuario no autorizado
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
     
 }
